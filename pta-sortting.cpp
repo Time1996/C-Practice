f333a5309f440a0621b919b72384e11f18f34c25
Time1996 <736874054@qq.com>
2019-08-05 22:40:09 +0800

    pta-sortting.cpp

--- pta-sortting.cpp
+++ pta-sortting.cpp
@@ -26,6 +26,73 @@ void Bubble_sort(long A[], int N)
     }
 }
 
+void Insert_sort(long A[], int N)
+{
+    //插入排序
+    int P, i;
+    for (P = 1; P < N; P++)
+    {
+        long temp = A[P];
+        for(i=P; i>=1 && temp<A[i-1]; i--)
+        {
+            A[i] = A[i-1];
+        }
+        A[i] = temp;
+    }
+}
+
+void Shell_sort(long A[], int N)
+{
+    //原始的希尔排序
+    int P, i, D;
+    for(D=N/2; D>=1; D/=2)//希尔增量序列
+    {
+        for(P=D; P<N; P++)//最后利用插入排序
+        {
+            long temp = A[P];
+            for(i=P; i>=D && temp<A[i-D]; i-=D)
+                A[i] = A[i-D];
+            A[i] = temp;
+        }
+    }
+}
+
+void ShellHib_sort(long A[], int N)
+{
+    //Hibbard增量序列的希尔排序
+    int k = log2(N), P, i, D;
+    for(D=pow(2, k)-1; D>-1 && k>=1; D=pow(2, --k)-1)//增量序列：Dk = 2k -1相邻元素互质
+    {
+        for(P=D; P<N; P++)
+        {
+            long temp = A[P];
+            for(i=P; i>=D && temp<A[i-D]; i-=D)
+                A[i] = A[i-D];
+            A[i] = temp;
+        }
+    }
+}
+
+void ShellSed_sort(long A[], int N)
+{
+    //Sedgewick增量序列的希尔排序
+    int D, P, j, i = 0;
+    int Sedgewick[] = {3905, 929, 505, 209, 109, 41, 19, 5, 1, 0};
+    for(D=Sedgewick[i]; D>=N; D=Sedgewick[i++]);
+    for(D=Sedgewick[i]; D>0; D=Sedgewick[++i])
+    {
+        for(P=D; P<N; P++)
+        {
+            long temp = A[P];
+            for(j=P; j>=D && temp<A[j-D]; j-=D)
+                A[j] = A[j-D];
+            A[j] = temp;
+        }
+    }
+}
+
+
+
 int main(int argc, char const *argv[])
 {
     int N;
